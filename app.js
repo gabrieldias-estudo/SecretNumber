7e8e50a1102648ae3ddf488bdc3272c56506f587
gabrieldias-estudo <gabrieldiasmenezes120306@gmail.com>
2025-10-24 17:10:26 -0300

    Adicionando total de tentativas e adicionando a funcionalidade de resetar o jogo.

--- app.js
+++ app.js
@@ -1,26 +1,45 @@
-let secretNumber=parseInt(Math.random()*10+1);
-
-
+let secretNumber=gerarNumeroAleatorio();
+let tentativas=1
+console.log(secretNumber);
 let mensagem=document.getElementById('resposta');
 
 function verificarChute(){
     let chute=parseInt(document.querySelector('input').value);
-    mensagem.innerHTML= definirMensagem(chute,secretNumber);
-    corDaMensagem(chute,secretNumber);
-}
-
-
-function definirMensagem(chute,secretNumber,){
+    let palavraTentativa= tentativas > 1 ? "tentativas" : "tentativa";
     if(chute == secretNumber){
-        return `Parabéns! Você acertou o número secreto ${secretNumber} !` ;
-    }else if(chute > secretNumber){
-        return `O número secreto é menor que ${chute}`;
+        definirMensagem(`Parabéns! Você acertou o número secreto ${secretNumber} em ${tentativas} ${palavraTentativa} !`,"#008500");
+        document.getElementById('reiniciar').removeAttribute('disabled');
     }else{
-        return `O número secreto é maior que ${chute}`;
-    }
+        if(chute > secretNumber){
+            definirMensagem(`O número secreto é menor que ${chute}`,"#ff3232ff");
+        }else{
+            definirMensagem(`O número secreto é maior que ${chute}`,"#ff3232ff");
+        }
+        tentativas++;
+        LimpaCampo();
+    } 
+        
 }
 
-function corDaMensagem(chute,secretNumber){
-    return mensagem.style.color= chute != secretNumber ? "#ff3232ff" : "#008500";
+function definirMensagem(texto,cor){
+    mensagem.innerHTML=texto;
+    mensagem.style.color=cor;
 }
+
+function gerarNumeroAleatorio(){
+    return parseInt(Math.random()*10+1);
+}
+
+function LimpaCampo(){
+    chute=document.querySelector('input');
+    chute.value='';
+}
+function resetGame(){
+    secretNumber=gerarNumeroAleatorio();
+    tentativas=1;
+    LimpaCampo();
+    definirMensagem('')
+    document.getElementById('reiniciar').setAttribute('disabled',true);
+}
+
 
